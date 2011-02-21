2869712dc1c947668304f0502a9ec468c4d13da7
Mason Wendell <mason@zivtech.com>
2011-02-21 12:16:35 -0500

    initial commit
    Signed-off-by: Mason Wendell <mason@zivtech.com>

---
+++ css/config.rb
@@ -0,0 +1,22 @@
+require 'susy'
+require 'fancy-buttons'  
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ""
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "../js"
+fonts_dir = "fonts"
+output_style = :nested
+# output_style = :compact
+# output_style = :expanded
+line_comments = false
+sass_options = {:debug_info => false} 
+preferred_syntax = :sass
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+
