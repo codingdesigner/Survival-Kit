c48f3ecb84ab354b0b4611bb08219ded2972c350
Mason Wendell <mason@zivtech.com>
2011-09-23 13:34:14 -0400

    moving sass source files

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "css/src"
+sass_dir = "sass"
 images_dir = "css/images"
 javascripts_dir = "js"
 fonts_dir = "css/type"
