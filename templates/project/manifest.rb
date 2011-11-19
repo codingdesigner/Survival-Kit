2f76a1c111008ac3853acd399584ac70a3072941
Mason Wendell <mason@zivtech.com>
2011-11-19 11:41:54 -0500

    moved generated html to extension folder

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -38,23 +38,26 @@ stylesheet 'partials/11-browser-adjustments/_modernizr.sass'
 stylesheet 'partials/11-browser-adjustments/_selectivizr.sass'
 # discover :stylesheets
 
-html 'index.html.haml'
+html 'files/html/index.html', :to => 'index.html'
+html 'files/html/404.html', :to => '404.html'
+html 'files/html/survivalkit/elements-common.html', :to => 'survivalkit/elements-common.html'
+html 'files/html/survivalkit/elements-html5.html', :to => 'survivalkit/elements-html5.html'
+html 'files/html/survivalkit/elements-ui-patterns-drupal.html', :to => 'survivalkit/elements-ui-patterns-drupal.html'
+html 'files/html/survivalkit/elements-ui-patterns.html', :to => 'survivalkit/elements-ui-patterns.html'
+html 'files/html/survivalkit/elements-typography.html', :to => 'survivalkit/elements-typography.html'
+html 'files/html/survivalkit/styletile/styletile.html', :to => 'survivalkit/styletile.html'
+
+html 'files/html/formalize_jquery_demo.html', :to => 'survivalkit/formalize/formalize_jquery_demo.html'
+html 'files/html/formalize_jquery_disabled.html', :to => 'survivalkit/formalize/formalize_jquery_disabled.html'
+html 'files/html/formalize_jquery_errors.html', :to => 'survivalkit/formalize/formalize_jquery_errors.html'
+
 html 'files/htaccess', :to => '.htaccess'
 html 'files/crossdomain.xml', :to => 'crossdomain.xml'
 html 'files/robots.txt', :to => 'robots.txt'
 html 'files/humans.txt', :to => 'humans.txt'
 html 'files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
 html 'files/favicon.ico', :to => 'favicon.ico'
-html 'files/html/404.html', :to => '404.html'
-html 'files/html/elements-common.html', :to => 'survivalkit/elements-common.html'
-html 'files/html/elements-html5.html', :to => 'survivalkit/elements-html5.html'
-html 'files/html/elements-ui-patterns-drupal.html', :to => 'survivalkit/elements-ui-patterns-drupal.html'
-html 'files/html/elements-ui-patterns.html', :to => 'survivalkit/elements-ui-patterns.html'
-html 'files/html/elements-typography.html', :to => 'survivalkit/elements-typography.html'
-html 'files/html/styletile.html', :to => 'survivalkit/styletile.html'
-html 'files/html/formalize_jquery_demo.html', :to => 'survivalkit/formalize/formalize_jquery_demo.html'
-html 'files/html/formalize_jquery_disabled.html', :to => 'survivalkit/formalize/formalize_jquery_disabled.html'
-html 'files/html/formalize_jquery_errors.html', :to => 'survivalkit/formalize/formalize_jquery_errors.html'
+
 discover :javascripts
 
 
