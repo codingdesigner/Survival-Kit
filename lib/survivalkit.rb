84771314839847ae34dcd4b8ebde1a117c19feef
Mason Wendell <mason@zivtech.com>
2012-05-25 00:51:05 -0400

    update gem definition

--- lib/survivalkit.rb
+++ lib/survivalkit.rb
@@ -1,9 +1,11 @@
 require 'compass'
+require 'susy'
+require 'breakpoint'
 Compass::Frameworks.register("survivalkit", :path => "#{File.dirname(__FILE__)}/..")
 
 module SurvivalKit
 
-  VERSION = "0.3.3"
-  DATE = "2012-05-22"
+  VERSION = "0.3.4"
+  DATE = "2012-05-25"
 
 end
