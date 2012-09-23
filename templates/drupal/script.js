dc5000daec4afd2f9bbd3bb0c134d777e30edb5a
Mason Wendell <mason@canarypromo.com>
2012-09-23 02:06:09 -0400

    clean out more js

--- templates/drupal/script.js
+++ templates/drupal/script.js
@@ -1,22 +1,7 @@
-/* Author:
-*  [your name here]
-*/
-
 $(document).ready(function(){
 
-  // Run Matt Kersley's jQuery Responsive menu plugin (see plugins-320.js)
-  if ($.fn.mobileMenu) {
-    $('ol#id').mobileMenu({
-      switchWidth: 768,                   // width (in px to switch at)
-      topOptionText: 'Choose a page',     // first option text
-      indentString: '&nbsp;&nbsp;&nbsp;'  // string for indenting nested items
-    });
-  }
+  // add some js, whydoncha?
 
-  // Run Mathias Bynens jQuery placeholder plugin (see plugins-320.js)
-  if ($.fn.placeholder) {
-    $('input, textarea').placeholder();
-  }
 });
 
 
