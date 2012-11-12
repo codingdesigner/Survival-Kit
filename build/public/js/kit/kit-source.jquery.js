74595b49b8343b08f7f374adc127a2fc8a14259c
Mason Wendell <mason@canarypromo.com>
2012-11-12 12:59:49 -0500

    styling navigation

--- build/public/js/kit/kit-source.jquery.js
+++ build/public/js/kit/kit-source.jquery.js
@@ -49,45 +49,57 @@
 * Collapse markup sections
 */
 (function($){
-  $('div.element').find('.element-title').append('<a href="#" class="fold-article fold-article-close" data-label="close"></a>');
+  $('div.element').find('.element-title').append('<a href="#" class="fold-article fold-article-close" data-label="close">close</a>');
   $('.fold-article').toggle(
     function() {
       $(this).parents('.element').addClass('closed');
       $(this).parent().siblings().slideUp();
-      $(this).attr('data-label', 'close').removeClass('fold-article-close').addClass('fold-article-open');
+      $(this)
+        .attr('data-label', 'open')
+        .html('open')
+        .removeClass('fold-article-close')
+        .addClass('fold-article-open');
     },
     function() {
       $(this).parents('.element').removeClass('closed');
       $(this).parent().siblings().slideDown();
-      $(this).attr('data-label', 'open').removeClass('fold-article-open').addClass('fold-article-close');
+      $(this)
+        .attr('data-label', 'close')
+        .html('close')
+        .removeClass('fold-article-open')
+        .addClass('fold-article-close');
     }
   );
   // element groups
-  $('.section-title').append('<a href="#" class="fold-section fold-section-close" data-label="fold section"></a>');
+  $('.section-title').append('<a href="#" class="fold-section fold-section-close" data-label="fold section">close</a>');
   $('.fold-section').toggle(
     function() {
       $(this)
         .attr('data-label', 'open')
+        .html('open')
+        .removeClass('fold-section-close').addClass('fold-section-open')
         .parent().siblings('.element').addClass('closed')
         .children(':not(.element-title)').slideUp().end()
         .find('.element-title').find('.fold-article-close')
-        .removeClass('fold-article-close').addClass('fold-article-open')
+          .removeClass('fold-article-close')
+          .addClass('fold-article-open')
+          .attr('data-label', 'open')
+          .html('open')
         ;
-      $(this)
-        .attr('data-label', 'open')
-        .removeClass('fold-section-close').addClass('fold-section-open');
     },
     function() {
       $(this)
         .attr('data-label', 'close')
+        .html('close')
+        .removeClass('fold-section-open').addClass('fold-section-close')
         .parent().siblings('.element').removeClass('closed')
         .children().slideDown().end()
         .find('.element-title').find('.fold-article-open')
-        .removeClass('fold-article-open').addClass('fold-article-close')
+          .removeClass('fold-article-open')
+          .addClass('fold-article-close')
+          .attr('data-label', 'close')
+          .html('close')
         ;
-      $(this)
-        .attr('data-label', 'close')
-        .removeClass('fold-section-open').addClass('fold-section-close');
     }
   );
 })(jQuery);
@@ -97,8 +109,11 @@
 * back to top links
 */
 (function($){
-  $('div.element').find('.element-title').append('<a href="#" class="back-to-top" data-label="top"></a>');
-  $('.section-title').append('<a href="#" class="back-to-top" data-label="top"></a>');
+  $('div.element')
+    .find('.element-title')
+    .append('<a href="#" class="back-to-top" data-label="top">top</a>');
+  $('.section-title')
+    .append('<a href="#" class="back-to-top" data-label="top">top</a>');
 })(jQuery);
 
 
