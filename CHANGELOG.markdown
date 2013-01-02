0209a1e3c8c1ed1b1cb1d3bac6269ac69e984660
Mason Wendell <mason@canarypromo.com>
2013-01-02 16:31:13 -0500

    fix link transition mixin

--- CHANGELOG.markdown
+++ CHANGELOG.markdown
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.0.beta.15 - January 2, 2013
+* fixed link transition mixin
+
 ## 1.0.beta.14 - December 3, 2012
 * font-face mixins now have an inline option
 
--- lib/survivalkit.rb
+++ lib/survivalkit.rb
@@ -8,7 +8,7 @@ Compass::Frameworks.register("survivalkit", :path => "#{File.dirname(__FILE__)}/
 
 module SurvivalKit
 
-  VERSION = "1.0.beta.14"
-  DATE = "2012-12-03"
+  VERSION = "1.0.beta.15"
+  DATE = "2013-01-02"
 
 end
